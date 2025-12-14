c8406692a976fa83597757b4d5e497b9b0eb2e57
Anurag910955 <145783931+Anurag910955@users.noreply.github.com>
2025-12-14 17:38:55 +0530

    updated frontend link

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://anurag910955-trueigtech-task-instagram.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
