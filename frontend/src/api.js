ef2008eedabe7c44bdc3037b1f36072935f9b4aa
Anurag910955 <145783931+Anurag910955@users.noreply.github.com>
2025-12-14 17:47:13 +0530

    fix frontend production issue

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://anurag910955-trueigtech-task-instagram.onrender.com"
+  baseURL: "https://anurag910955-trueigtech-task-instagram.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = token;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
   return req;
 });
 
